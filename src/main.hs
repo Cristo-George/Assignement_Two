ee1436a3a79241c4a43a803f36ff7a5d921fbe44
Alfred-Jijo <alfredjijo28@gmail.com>
2024-03-06 15:03:24 +0000

    nuh uh

--- src/main.hs
+++ src/main.hs
@@ -1,3 +1,10 @@
 main :: IO ()
 main = do
-  putStr "Ur mom"
+  let anodeVoltage = [2000, 2500, 3000, 3500, 4000]
+  let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050]
+
+  putStrLn "Anode Voltage:"
+  print anodeVoltage
+
+  putStrLn "First Ring Diameter:"
+  print firstRingDiameter
