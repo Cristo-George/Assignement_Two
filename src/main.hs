4c799ba911bb6be07e307fedd2831793affc5023
Alfred-Jijo <alfredjijo28@gmail.com>
2024-03-06 15:55:56 +0000

    lambda function

--- src/main.hs
+++ src/main.hs
@@ -2,11 +2,10 @@ main :: IO ()
 main = do
   let anodeVoltage = [2000, 2500, 3000, 3500, 4000] -- Voltage
   let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050] -- Ring Diameter
-  
-  let vInverse = map (\x -> 1 / x) anodeVoltage -- Makes list 1/V
+  let vInverse = map (1 /) anodeVoltage -- Makes list 1/V
   let dSquare = map (\x -> x * x) firstRingDiameter -- Makes list D^2
-  
-  let gradient = (vInverse!![-1] - vInverse !![0]) / (dsquare!![-1] - dsquare!![0])
+  let dInverseSquare = map (1 /) dSquare
+  let gradient = zipWith (*) vInverse dInverseSquare
 
   putStrLn "Anode Voltage:"
   print anodeVoltage
