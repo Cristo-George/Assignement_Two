a4a6100091c989f3995f1effec60fcf25f1a8b7b
Alfred-Jijo <alfredjijo28@gmail.com>
2024-03-06 15:49:06 +0000

    print gradient

--- src/main.hs
+++ src/main.hs
@@ -2,11 +2,10 @@ main :: IO ()
 main = do
   let anodeVoltage = [2000, 2500, 3000, 3500, 4000] -- Voltage
   let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050] -- Ring Diameter
-  
-  let vInverse = map (\x-> 1/x)anodeVoltage -- Makes list 1/V
-  let dSquare = map (\x-> x*x )firstRingDiameter -- Makes list D^2
-  let dInverseSquare = map (\x -> 1/x) dSqaure
-  let gradient = zipWidth (*) vInverse dInverseSquare
+  let vInverse = map (\x -> 1 / x) anodeVoltage -- Makes list 1/V
+  let dSquare = map (\x -> x * x) firstRingDiameter -- Makes list D^2
+  let dInverseSquare = map (\x -> 1 / x) dSquare
+  let gradient = zipWith (*) vInverse dInverseSquare
 
   putStrLn "Anode Voltage:"
   print anodeVoltage
@@ -20,4 +19,5 @@ main = do
   putStrLn "dSquare"
   print dSquare
 
-  
+  putStrLn "grad"
+  print gradient
