fded22e467f9893d380f31e985dd4b35ff9c5583
Cristo-George <162441548+Cristo-George@users.noreply.github.com>
2024-03-06 15:21:19 +0000

    Updateder main.hs

--- src/main.hs
+++ src/main.hs
@@ -2,10 +2,13 @@ main :: IO ()
 main = do
   let anodeVoltage = [2000, 2500, 3000, 3500, 4000]
   let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050]
-  let Vinverse = map $ 1/anodeVoltage[0..]
+  let V_Inverse = map (x-> x*-1)anodeVoltage
 
   putStrLn "Anode Voltage:"
   print anodeVoltage
 
   putStrLn "First Ring Diameter:"
   print firstRingDiameter
+
+  putStrLn "V_Inverse:"
+  print V_Inverse
