cbd6f919c9278df41a433080b08615a35951c245
Cristo-George <162441548+Cristo-George@users.noreply.github.com>
2024-03-06 15:18:57 +0000

    Update main.hs

--- src/main.hs
+++ src/main.hs
@@ -2,6 +2,7 @@ main :: IO ()
 main = do
   let anodeVoltage = [2000, 2500, 3000, 3500, 4000]
   let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050]
+  let Vinverse = map $ 1/anodeVoltage[0..]
 
   putStrLn "Anode Voltage:"
   print anodeVoltage
