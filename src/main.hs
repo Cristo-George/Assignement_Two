f7f7b20f4d7609a9794a63f1a863093b07be91cb
Cristo-George <162441548+Cristo-George@users.noreply.github.com>
2024-03-06 16:01:33 +0000

    Update main.hs

--- src/main.hs
+++ src/main.hs
@@ -2,9 +2,10 @@ main :: IO ()
 main = do
   let anodeVoltage = [2000, 2500, 3000, 3500, 4000] -- Voltage
   let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050] -- Ring Diameter
+  
   let vInverse = map (1 /) anodeVoltage -- Makes list 1/V
   let dSquare = map (\x -> x * x) firstRingDiameter -- Makes list D^2
-  let dInverseSquare = map (1 /) dSquare
+  
   let gradient = ((last vInverse) - (head vInverse)) / ((last dSquare) - (head dSquare))
 
   putStrLn "Anode Voltage:"
