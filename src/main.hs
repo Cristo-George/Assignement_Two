331fe5c7af49e07d4b8b472bf127a1f2f036195f
Cristo-George <162441548+Cristo-George@users.noreply.github.com>
2024-03-06 15:38:10 +0000

    mapping

--- src/main.hs
+++ src/main.hs
@@ -3,8 +3,8 @@ main = do
   let anodeVoltage = [2000, 2500, 3000, 3500, 4000] -- Voltage
   let firstRingDiameter = [0.071, 0.063, 0.058, 0.053, 0.050] -- Ring Diameter
   
-  let vInverse = map (x-> x*-1)anodeVoltage -- Makes list 1/V
-  let dSquare = map (x-> x*x )firstRingDiameter -- Makes list D^2
+  let vInverse = map (\x-> 1/x)anodeVoltage -- Makes list 1/V
+  let dSquare = map (\x-> x*x )firstRingDiameter -- Makes list D^2
 
   putStrLn "Anode Voltage:"
   print anodeVoltage
